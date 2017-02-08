c3d31ba8dc90acd605c8c698cd23a077d9381954
Gavin Smith <gavinasmith@me.com>
2017-02-08 14:43:17 -0500

    update application_pdf (belongs with previous PDF commit)

--- app/pdfs/application_pdf.rb
+++ app/pdfs/application_pdf.rb
@@ -6,10 +6,21 @@ class ApplicationPDF < Prawn::Document
       page_layout: :portrait
     )
     super(opts)
-    font('Helvetica')
+    # font('Helvetica')
+    font(self.class.default_font_name,
+      size: self.class.default_font_size
+    )
   end
 
   def filename
     self.class.to_s.titleize.gsub(/(PDF|Document)$/, '').chop
+  end
+
+  def self.default_font_name
+    'Courier'
+  end
+
+  def self.default_font_size
+    8
   end
 end
