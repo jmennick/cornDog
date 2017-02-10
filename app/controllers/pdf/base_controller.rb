5722facc34c43b1ab41d20124a95083d740b13e4
Gavin Smith <gavinasmith@me.com>
2017-02-10 16:31:33 -0500

    fixed issue with PDF controller naming

--- app/controllers/pdf/base_controller.rb
+++ app/controllers/pdf/base_controller.rb
@@ -1,4 +1,4 @@
-class PDF::BaseController < ApplicationController::API
+class PDF::BaseController < ActionController::Base
   def send_pdf(pdf_or_class, filename: nil)
       pdf = case pdf_or_class
       when Class then pdf_or_class.new
