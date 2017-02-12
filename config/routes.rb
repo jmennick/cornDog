d5ac0b4afb00d58e704fb7e79441d14df32ef336
Gavin Smith <gavinasmith@me.com>
2017-02-12 18:02:09 -0500

    redirect root to accounts list (for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: redirect('/accounts')
   resources :accounts
 
-  namespace :pdf, format: :pdf do
-    get :chart_of_accounts, to: 'chart_of_accounts#chart_of_accounts'
-    get :test, to: 'test#test'
-  end
+  # namespace :pdf, format: :pdf do
+  #   get :chart_of_accounts, to: 'chart_of_accounts#chart_of_accounts'
+  #   get :test, to: 'test#test'
+  # end
 
   # scope 'api', format: :json do
   #   jsonapi_resources :accounts
