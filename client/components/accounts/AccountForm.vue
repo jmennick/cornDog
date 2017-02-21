d50c8e37574a73190b2870796267b4467e3e7ec2
Gavin Smith <gavinasmith@me.com>
2017-02-20 21:22:05 -0500

    Fixed typo

--- client/components/accounts/AccountForm.vue
+++ client/components/accounts/AccountForm.vue
@@ -1,43 +1,65 @@
 <template>
-  <div>
-  <h1>This is an account form</h1>
-  <form>
-    <b-form-input v-model="resource.name"
-                  type="text"
-                  placeholder="Account Name"></b-form-input>
-    <!--Add select for acccount type-->
-
-    <!--/Add select for acccount type-->
-    <b-form-input v-model="resource.balance"
-                  type="number"
-                  placeholder="Initial Balance"></b-form-input>
-    <b-form-input v-model="resource.code"
-                  type="number"
-                  placeholder="Account Code"></b-form-input>
-    <b-form-input v-model="resource.order"
-                  type="number"
-                  placeholder="Order"></b-form-input>
-    <b-form-input v-model="resource.description"
-                  type="text"
-                  placeholder="Description"></b-form-input>
-    <label>
-    <input type="checkbox" class="form-check-input">
-      Active
-    </label>
-  </form>
+  <div class="container-fluid">
+    <form>
+      <b-form-fieldset label="Name">
+        <b-form-input v-model="name" type="text" placeholder="Account Name"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset label="Kind">
+        <select class="form-control" v-model="kind">
+          <optgroup v-for="t in types" :label="t.label">
+            <option v-for="k in t.kinds" :value="k.value">
+              {{k.label}}
+            </option>
+          </optgroup>
+        </select>
+      </b-form-fieldset>
+      <b-form-fieldset label="Initial Balance">
+        <b-form-input v-model="initial_balance" type="number" placeholder="Initial Balance"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset label="Code">
+        <b-form-input v-model="code" type="number" placeholder="Account Code"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset label="Order">
+        <b-form-input v-model="order" type="number" placeholder="Order"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset label="Description">
+        <b-form-input v-model="description" type="text" placeholder="Description"></b-form-input>
+      </b-form-fieldset>
+      <b-form-fieldset label="Active">
+        <b-form-checkbox v-model="active">Active</b-form-checkbox>
+      </b-form-fieldset>
+    </form>
   </div>
 </template>
 
-<script>;
+<script>
 import {mapState} from 'vuex'
 import resourceFormMixin from '~assets/js/mixins/resourceFormMixin'
+
+const mapProp = (pname)=> ({
+  get() {
+    return this.resource[pname]
+  },
+  set(newValue) {
+    let resource = this.resource
+    resource[pname] = newValue
+    this.saveData(resource)
+  }
+})
 
 export default {
   mixins: [resourceFormMixin],
   computed: {
     ...mapState({
-        types: ({resource})=> resource.meta.kinds_grouped
-    })
+        types: ({resource})=> resource.meta.kinds_grouped,
+    }),
+    name: mapProp('name'),
+    kind: mapProp('kind'),
+    // initial_balance: mapProp('initial_balance'),
+    code: mapProp('code'),
+    order: mapProp('order'),
+    description: mapProp('description'),
+    active: mapProp('active')
   }
 }
 </script>
