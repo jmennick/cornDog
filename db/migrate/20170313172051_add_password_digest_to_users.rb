8766c3f082003d68a02cef51367e95e45504c391
Gavin Smith <gavinasmith@me.com>
2017-03-14 01:56:36 -0400

    The password an idiot would keep on his luggage

--- db/migrate/20170313172051_add_password_digest_to_users.rb
+++ db/migrate/20170313172051_add_password_digest_to_users.rb
@@ -1,5 +1,6 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :password_digest, :string, null: false, default: ''
+    User.all.each{|u| u.update! password: '12345'}
   end
 end
